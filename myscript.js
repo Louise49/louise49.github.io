d1b080861e3f6639d6dfab7b62e07d00c1bbc235
Louise49 <43102525+Louise49@users.noreply.github.com>
2018-10-15 00:55:56 +0800

    change java

--- myscript.js
+++ myscript.js
@@ -14,6 +14,17 @@ function ask_for_x_n_y() { 
   x.style.color = "red"; 
   document.getElementById("demo").innerHTML = "The answer is " + z; 
 }
+
+function ask_xy() { 
+    var x = document.getElementById("myForm").elements[0].value;
+    var y = document.getElementById("myForm").elements[1].value;
+    var z = compute_x_plus_y(x,y);
+    var x = document.getElementById("demo"); 
+    x.style.fontSize = "25px"; 
+    x.style.color = "red"; 
+    document.getElementById("demo").innerHTML = "The answer is " + z; 
+  }
+
 function show_topic() { 
   var x = document.getElementById("welcome"); 
   x.style.fontSize = "25px"; 
