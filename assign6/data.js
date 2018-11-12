7539f83ffa68283f138ca222904d815a458af2f6
Louise49 <43102525+Louise49@users.noreply.github.com>
2018-11-12 16:42:23 +0800

    combine js file
    combine js file

--- assign6/data.js
+++ assign6/data.js
@@ -74,4 +74,41 @@ $(document).ready(function(){
 
         }); // end .getJSON
      }); // end button
+
+     $("button#get_age").click(function() {
+
+       var table3_items = [];
+       var i = 0;
+       var airtable_read_endpoint =
+       "https://api.airtable.com/v0/appdKlfv4HAjmlZGI/Json?api_key=key3P4gTMtrDY1ylx";
+       var table3_dataSet = [];
+       $.getJSON(airtable_read_endpoint, function(result) {
+              $.each(result.records, function(key,value) {
+                  table3_items = [];
+                      table3_items.push(value.fields.Companies);
+                      table3_items.push(value.fields.Average_age);
+                      table3_dataSet.push(table3_items);
+                      console.log(table3_items);
+               }); // end .each
+               console.log(table3_dataSet);
+
+               var chart = c3.generate({
+                   data: {
+                       columns: table3_dataSet,
+                   },
+               });
+                   axis: {
+                       x: {
+                           label: 'Company'
+                       },
+                       y: {
+                           label: 'Average Age'
+                       },
+                   }
+               });
+
+        }); // end .getJSON
+     }); // end button
+
+
 }); // document ready
