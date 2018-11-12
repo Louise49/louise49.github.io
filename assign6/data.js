cd098e16f3654b26ffa4e65f50166093c1a0318e
Louise49 <43102525+Louise49@users.noreply.github.com>
2018-11-12 17:16:02 +0800

    refine charts
    refine charts

--- assign6/data.js
+++ assign6/data.js
@@ -42,36 +42,5 @@ $(document).ready(function(){
             } );
        }); // end .getJSON
 
-       var table2_items = [];
-       var i = 0;
-       var airtable_read_endpoint =
-       "https://api.airtable.com/v0/appdKlfv4HAjmlZGI/Json?api_key=key3P4gTMtrDY1ylx";
-       var table2_dataSet = [];
-       $.getJSON(airtable_read_endpoint, function(result) {
-              $.each(result.records, function(key,value) {
-                  table2_items = [];
-                      table2_items.push(value.fields.Companies);
-                      table2_items.push(value.fields.Weibo_Fans);
-                      table2_dataSet.push(table2_items);
-                      console.log(table2_items);
-               }); // end .each
-               console.log(table2_dataSet);
-
-              var chart = c3.generate({
-                   data: {
-                       columns: table2_dataSet,
-                       type : 'bar'
-                   },
-                   axis: {
-                       x: {
-                           label: 'Company'
-                       },
-                       y: {
-                           label: 'Weibo Fans Amount'
-                       },
-                   }
-               });
-
-        }); // end .getJSON
      }); // end button
 }); // document ready
