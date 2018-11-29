43d89a7253937024fafcfe4c12d9944326f39296
Louise49 <43102525+Louise49@users.noreply.github.com>
2018-11-29 23:48:50 +0800

    update storymap
    update storymap

--- final/storymap.js
+++ final/storymap.js
@@ -13,48 +13,48 @@ var storymap_data = {
         text: "<p>涵盖国内九大顶尖商业配音团队。最新资讯，一站享有。</p>"
       },
       media: {
-        url: "https://dl.airtable.com/sJG8lNQRcmbY4bjJhbw8_logo.png",
-        credit: "香港",
+        url: "https://dl.airtable.com/AB0Q6a3aSlKgFPDbkx6k_bg1.jpg",
+        credit: "2018.12",
         caption: ""
       }
     },
      {
-      date: "2016",
+      date: "2005",
       text: {
         headline: "729声工场",
-        text: "<p> 创始于2005年7月29日，原名729配音组。2016年7月29日，729配音组正式更名729声工场，发展成为一支专业配音团队。著名配音导演及演员张杰为该品牌创始人之一。参与并制作诸多动画、 影视、游戏、广播剧等作品。 代表作：动画《狐妖小红娘》、《全职高手》、游戏《仙剑奇侠传》系列等。</p>"
+        text: "<p> 创始于2005年7月29日，原名729配音组。2016年7月29日，729配音组正式更名729声工场，发展成为一支专业配音团队。著名配音导演及演员张杰为该品牌创始人之一。参与并制作诸多动画、 影视、游戏、广播剧等作品。 <br>代表作：动画《狐妖小红娘》、《全职高手》、游戏《仙剑奇侠传》系列等。</p>"
       },
       location: {
         name: "729声工场",
-        icon: "https://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/blue-pushpin.png",
+        icon: "https://dl.airtable.com/Pti3l6tdQMypu15hRJ5S_red-pushpin.png",
         lat: 39.9587664,
         lon: 116.3579938,
         zoom: 16,
         line: true
       },
       media: {
-        url: "https://dl.airtable.com/a6LGXVK5SoKMfjalKoib_729logo.png",
+        url: "https://www.youtube.com/watch?v=6BtRxuJADps",
         type: 		"image",
         credit: "北京",
         caption: "以声动人，用心做事。"
       }
     },
     {
-     date: "2015",
+     date: "2009",
      text: {
        headline: "光合积木工作室",
-       text: "<p> 2015年成立，是由资深配音演员姜广涛创建的配音团队。以影视配音为主要业务，担任众多影视剧的对白配音工作，并参与动画、游戏、有声漫画、广播剧等各类有声作品的创作。代表作有电视剧《大唐荣耀》《择天记》《思美人》、电影《港囧》《微微一笑很倾城》《从你的全世界路过》、动画《黑白无双》《超神学院之黑甲》《少年锦衣卫》、广播剧《芈月传》等。</p>"
+       text: "<p> 成立于2009年，由国内知名配音导演、配音演员姜广涛创立，由其成员执导及配音的作品包括电影《港囧》《风声》《从你的全世界路过》，电视剧《琅琊榜》《宫》《美人心计》《择天记》《幻城》，动漫《魁拔》《大鱼海棠》《少年锦衣卫》，广播剧《很想很想你》《蝉女》，系列游戏《仙剑奇侠传》在内的百余部作品。<br>近年来，光合积木工作室担任众多影视、动画、游戏的对白配音工作，并参与有声小说、有声漫画、广播剧等各类有声作品的创作。</p>"
      },
      location: {
        name: "光合积木工作室",
-       icon: "https://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/blue-pushpin.png",
+       icon: "https://dl.airtable.com/Pti3l6tdQMypu15hRJ5S_red-pushpin.png",
        lat: 39.9676207,
        lon: 116.5326328,
        zoom: 16,
        line: true
      },
      media: {
-       url: "https://dl.airtable.com/a6LGXVK5SoKMfjalKoib_729logo.png",
+       url: "https://dl.airtable.com/H4W1UFToSwKXug6hunj4_Voicegem.png",
        type: 		"image",
        credit: "北京",
        caption: "阳光，可以孕育生命；积木，可以无限组合；声音的积木，把这个世界说给你听。"
@@ -64,23 +64,65 @@ var storymap_data = {
       date: "2014",
       text: {
         headline: "北斗企鹅工作室",
-        text: "<p> 成立于2014年，是一家专注于ACG（动画，漫画及配音）领域的专业声优公司。团队目前拥有专职声优艺人四十余名，并与其他业内艺人保持良好的合作关系。 动画游戏以及新媒体属于新兴产业，北斗企鹅以保证作品质量和诚意为核心理念。在选人方面，以优秀的年轻配音演员为主，成熟配音演员为辅，从而使整个公司的人员构成多元化、立体化，可以更快的适应时代要求和市场要求，从而更好的完成项目，与市场对接。</p> "
+        text: "<p> 成立于2014年，是一家专注于ACG（动画，漫画及配音）领域的专业声优公司。团队目前拥有专职声优艺人四十余名，并与其他业内艺人保持良好的合作关系。 动画游戏以及新媒体属于新兴产业，北斗企鹅以保证作品质量和诚意为核心理念。<br>在选人方面，以优秀的年轻配音演员为主，成熟配音演员为辅，从而使整个公司的人员构成多元化、立体化，可以更快的适应时代要求和市场要求，从而更好的完成项目，与市场对接。</p> "
       },
       location: {
         name: "北斗企鹅工作室",
-        ICON:"https://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/blue-pushpin.png",
+        icon:"https://dl.airtable.com/Pti3l6tdQMypu15hRJ5S_red-pushpin.png",
         lat: 39.9082031,
         lon: 116.6039003,
         zoom: 16,
         line: true
       },
       media: {
-        url: "http://pg.com.cuhk.edu.hk/wp-content/uploads/2014/09/banner_mediaedu-en-1165x657.jpg",
+        url: "http://triopen.com/images/intro-logo.png",
         type: "image",
-        credit: "CUHK",
-        caption: "CUHK School of Journalism and Communication."
+        credit: "北京",
+        caption: "奇迹，正在发声。"
       }
+    },
+    {
+     date: "2017",
+     text: {
+       headline: "冠声文化",
+       text: "<p> 成立于2017年，冠声的声优团队分为两大阵营，以北京为阵地的声优团队会主要生产个性化的音频产品和担当出品或承制作品的重要角色，以全国范围内建立的冠声声优联盟，会接到和各平台合作的音频产品录制及部分作品的配音角色任务。</p> "
+     },
+     location: {
+       name: "北斗企鹅工作室",
+       icon:"https://dl.airtable.com/Pti3l6tdQMypu15hRJ5S_red-pushpin.png",
+       lat: 39.8291191,
+       lon: 116.1211347,
+       zoom: 16,
+       line: true
+     },
+     media: {
+       url: "https://www.youtube.com/watch?v=Vn95Fhoipwg",
+       type: "image",
+       credit: "北京",
+       caption: "用声音碰撞心灵，让耳朵谈一场恋爱！"
+     }
+   },
+   {
+    date: "2013",
+    text: {
+      headline: "音熊联萌",
+      text: "<p> 由知名配音演员谢添天、冯骏骅、夏磊、沈达威和鬼月于2013年2月5日共同创办，是以动漫游戏音频后期，声优经济活动及偶像化，泛二次元IP创作与孵化为主要业务方向的有爱团体。<br>音熊联萌现旗下拥有国内知名声优艺人近二十人，主要二次元向作品有：《秒速五厘米》、《游戏人生》、《斗破苍穹》、《择天记》、《妖怪名单》、《纳米核心》、《全职法师》、《中国惊奇先生》、《王牌御史》、《馒头日记》、《太乙仙魔录》等；游戏《天谕》、《剑网三》、《天下3》、《古墓丽影崛起》、《秦时明月》、《飞刀又见飞刀》等。2017年成立首支声优偶像少女组合V17。</p> "
+    },
+    location: {
+      name: "音熊联萌",
+      icon:"https://dl.airtable.com/Pti3l6tdQMypu15hRJ5S_red-pushpin.png",
+      lat: 31.2443085,
+      lon: 120.9162903,
+      zoom: 16,
+      line: true
+    },
+    media: {
+      url: "http://www.sound-u.cn/imgs/logo.png",
+      type: "image",
+      credit: "上海",
+      caption: "熊窝饲养员“干脆面”专属工作间。"
     }
+  },
     ] // end of slides
   }
 }
